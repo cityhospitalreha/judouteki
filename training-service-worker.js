2b1881373cfae4b1cfe9defb138fd1d0ec39fa78
cityhospitalreha <city_hospital_reha@icloud.com>
2026-06-12 14:18:59 +0900

    Add files via upload

---
+++ training-service-worker.js
@@ -0,0 +1,46 @@
+const CACHE_NAME = 'training-app-v1';
+const urlsToCache = [
+  './training.html',
+  './training-manifest.json',
+  './training-icon-192.png',
+  './training-icon-512.png'
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(urlsToCache);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.filter(function(name) {
+          return name !== CACHE_NAME;
+        }).map(function(name) {
+          return caches.delete(name);
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request).then(function(networkResponse) {
+        return caches.open(CACHE_NAME).then(function(cache) {
+          cache.put(event.request, networkResponse.clone());
+          return networkResponse;
+        });
+      });
+    }).catch(function() {
+      return caches.match('./training.html');
+    })
+  );
+});
